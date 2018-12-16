c23ee28ca100a6cf1cf583a83385fe44ae7cfeff
Daniel Cabral Correa <daniel.liboos@gmail.com>
2018-12-16 13:44:12 -0200

    update

--- codigo/coapLD.py
+++ codigo/coapLD.py
@@ -64,7 +64,7 @@ class DELTA(Enum): #opção delta
 	LOCATION_QUERY =  b'\x14'
 	PROXY_URI =  b'\x23'
 	PROXY_SCHEME =  b'\x27'
-	SIZE1 = B'\x3C'
+	SIZE1 = b'\x3C'
 	
 class coap:
 
@@ -82,77 +82,67 @@ class coap:
 		self.acesscode = b'\xff' # playload_mac e sempre ff.
 		self.payload = b''
 		self.quadro = b''
+		self.metodo = 'separado'
+		self.caminho
+		self.porta = int(5683)
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		
-	def QUADRO(self):
-		self.quadro = b''
-		self.quadro += ((self.versao[0] << 6) | (self.tipo[0] << 4) | (self.tkl[0])).to_bytes(1, byteorder='big')
-		self.quadro += self.code
-		self.quadro += self.messageID1
-		self.quadro += self.messageID2
-		self.quadro += ((self.optionsdelta[0] << 4) | (self.optionslength)).to_bytes(1, byteorder='big')
-		self.quadro += self.options
-		if ((self.code  == CODE_0.POST.value) or (self.code  == CODE_0.PUT.value)):
-		  self.quadro += bytes([self.acesscode])
-		self.quadro += self.payload
-		print(self.quadro)
 		
 	def recebe(self, quadro_recebido):
 		primeiro_byte = quadro_recebido[0]
-		versao_rec = primeiro_byte & ((2**7)+(2**6))
-		tipo_rec = primeiro_byte & ((2**5)+(2**4))
-		tkl_rec = primeiro_byte & ((2**3)+(2**2)+(2**1)+(2**0))
+		versao_rec = primeiro_byte & ((2 ** 7) + (2 ** 6))
+		tipo_rec = primeiro_byte & ((2 ** 5) + (2 ** 4))
+		tkl_rec = primeiro_byte & ((2 ** 3) + (2 ** 2) + (2 ** 1) + (2 ** 0))
 		quadro_recebido = quadro_recebido[1:]
-		if(versao_rec == (2**6)):
-			if(tipo_rec == (2**5)):
+		if (versao_rec == (2 ** 6)):
+			if (tipo_rec == (2 ** 5)):
 				codigo_rec = quadro_recebido[0]
 				quadro_recebido = quadro_recebido[1:]
-				if(codigo_rec == 128):
+				if (codigo_rec == 128):
 					return '4.00 - Bad Request'
-				if(codigo_rec == 129):
+				if (codigo_rec == 129):
 					return '4.01 - Unauthorized'
-				if(codigo_rec == 130):
+				if (codigo_rec == 130):
 					return '4.02 - Bad Option'
-				if(codigo_rec == 131):
+				if (codigo_rec == 131):
 					return '4.03 - Forbidden'
-				if(codigo_rec == 132):
+				if (codigo_rec == 132):
 					return '4.04 - Not Found'
-				if(codigo_rec == 133):
+				if (codigo_rec == 133):
 					return '4.05 - Method Not Allowed'
-				if(codigo_rec == 134):
+				if (codigo_rec == 134):
 					return '4.06 - Not Acceptable'
-				if(codigo_rec == 140):
+				if (codigo_rec == 140):
 					return '4.12 - Precondition Failed'
-				if(codigo_rec == 141):
+				if (codigo_rec == 141):
 					return '4.13 - Request Entity Too Large'
-				if(codigo_rec == 143):
+				if (codigo_rec == 143):
 					return '4.15 - Unsupported Content-Format'
-				if(codigo_rec == 160):
+				if (codigo_rec == 160):
 					return '5.00 - Internal Server Error'
-				if(codigo_rec == 161):
+				if (codigo_rec == 161):
 					return '5.01 - Not Implemented'
-				if(codigo_rec == 162):
+				if (codigo_rec == 162):
 					return '5.02 - Bad Gateway'
-				if(codigo_rec == 163):
+				if (codigo_rec == 163):
 					return '5.03 - Service Unavailable'
-				if(codigo_rec == 164):
+				if (codigo_rec == 164):
 					return '5.04 - Gateway Timeout'
-				if(codigo_rec == 165):
+				if (codigo_rec == 165):
 					return '5.05 - Proxying Not Supported'
-				if(codigo_rec >= 65 and codigo <= 69):
+				if (codigo_rec >= 65 and codigo <= 69):
 					messageID_rec = quadro_recebido[0:2]
 					quadro_recebido = quadro_recebido[2:]
 					messadeID = b''
 					messadeID += self.messageID1
 					messadeID += self.messageID2
-					if(messageID_rec == messageID):	
+					if (messageID_rec == messageID):
 						quadro_recebido = quadro_recebido[tkl_rec:]
 						aux = True
 						while aux:
 							op_delta_length = quadro_recebido[0]
 							quadro_recebido = quadro_recebido[1:]
-							op_delta = op_delta_length & ((2**7)+(2**6)+(2**5)+(2**4))
-							op_length = op_delta_length & ((2**3)+(2**2)+(2**1)+(2**0))
+							op_delta = op_delta_length & ((2 ** 7) + (2 ** 6) + (2 ** 5) + (2 ** 4))
+							op_length = op_delta_length & ((2 ** 3) + (2 ** 2) + (2 ** 1) + (2 ** 0))
 							op_delta = quadro_recebido >> 4
 							if (op_delta == 13):
 								op_delta = quadro_recebido[0]
@@ -166,7 +156,7 @@ class coap:
 								else:
 									aux = False
 								payload = quadro_recebido
-							if (op_delta < 13):		
+							if (op_delta < 13):
 								if (op_length == 13):
 									op_length = quadro_recebido[0]
 									quadro_recebido = quadro_recebido[1:]
@@ -174,91 +164,178 @@ class coap:
 									op_length = quadro_recebido[0:2]
 									quadro_recebido = quadro_recebido[2:]
 								if (op_length == 15):
-									return (ERRO)
+									return ('ERRO')
 								option = quadro_recebido[0:op_length]
-						if(codigo_rec == 65):
+						if (codigo_rec == 65):
 							return '2.01 - Created ' + payload.decode("utf-8")
-						if(codigo_rec == 66):
+						if (codigo_rec == 66):
 							return '2.02 - Deleted ' + payload.decode("utf-8")
-						if(codigo_rec == 67):
+						if (codigo_rec == 67):
 							return '2.03 - Valid ' + payload.decode("utf-8")
-						if(codigo_rec == 68):
+						if (codigo_rec == 68):
 							return '2.04 - Changed ' + payload.decode("utf-8")
-						if(codigo_rec == 69):
+						if (codigo_rec == 69):
 							return '2.05 - Content ' + payload.decode("utf-8")
 					return 'Erro - Mensagem Id não compativel'
 				return 'Codigo desconhecido'
 			return 'Tipo não esperado'
 		return 'versao inesistente'
+		
+	def QUADRO(self, recurso):
+		self.quadro = b''
+		self.quadro += ((self.versao[0] << 6) | (self.tipo[0] << 4) | (self.tkl[0])).to_bytes(1, byteorder='big')
+		self.quadro += self.code
+		self.quadro += self.messageID1
+		self.quadro += self.messageID2
+		if(self.metodo == separado):
+			lista_recurso = recurso.split('/')
+			for uri_path in lista_recurso:
+				self.optionsdelta = DELTA.URI_PATH.value[0] - self.optionsdelta
+				self.optionslength = len(uri_path)
+				quadro += (self.optionsdelta << 4 | self.optionslength).to_bytes(1, byteorder='big')
+				quadro += str.encode(uri_path)
+			if ((self.code  == CODE_0.POST.value) or (self.code  == CODE_0.PUT.value)):
+				self.quadro += bytes([self.acesscode])
+				self.quadro += self.payload
+			return (quadro)		
+		if(self.metodo == direto):
+			if (recurso[0:4] != 'coap'):
+				return 'ERRO'
+			parametros = recurso[4:].split('/')
+			addr = parametros[0].split(':')
+			self.optionslength = len(addr[0])
+			if (len(addr) > 1):
+				self.porta = int(addr[1]
+			addr = addr[0].split('.')
+			if (not(len(addr)==4 and addr[0].isnumeric and addr[1].isnumeric and addr[2].isnumeric and addr[3].isnumeric)):
+				self.optionsdelta = DELTA.URI_HOST.value[0] - optionsdelta
+				quadro += (self.optionsdelta << 4 | self.optionslength).to_bytes(1, byteorder='big')
+				quadro += str.encode(addr)
+			lista_recurso = parametros[1:]
+			for uri_path in lista_recurso:
+				self.optionsdelta = DELTA.URI_PATH.value[0] - self.optionsdelta
+				self.optionslength = len(uri_path)
+				quadro += (self.optionsdelta << 4 | self.optionslength).to_bytes(1, byteorder='big')
+				quadro += str.encode(uri_path)
+			if ((self.code  == CODE_0.POST.value) or (self.code  == CODE_0.PUT.value)):
+				self.quadro += bytes([self.acesscode])
+				self.quadro += self.payload
+			return (quadro)
 
-	def GET(self, uri_path, server_adress, port):
+	def GET(self, recurso, server_adress, port):
 		self.tipo = T.CONFIRMABLE.value
 		self.code = CODE_0.GET.value
-		self.optionsdelta = DELTA.URI_HOST.value
-		self.optionslength = len(uri_path)
-		self.options = uri_path
 		self.payload = b''
-		self.QUADRO()
+		self.caminho = server_adress
+		self.porta = port
+		self.metodo = 'separado'
+		self.QUADRO(recurso)
 		
-		#resource_list = resource.split('/')
-		#for uri_path in  resource_list:
-		#	if(self.opcao_delta > 0):
-		#		self.opcao_delta -= OPTIONS_DELTA.URI_PATH.value[0]
-		#	else:
-		#		self.opcao_delta = OPTIONS_DELTA.URI_PATH.value[0]
-		#	if(self.opcao_delta < 0):
-		#		self.opcao_delta = 0
-		#		
-		#	self.quadro += (self.opcao_delta << 4 | len(uri_path)).to_bytes(1, byteorder='big')
-		#	print(uri_path.encode())
-		#self.quadro += str.encode(uri_path)
-
-		self.sock.sendto(self.quadro, (server_adress, port))
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
+		data, addr = self.sock.recvfrom(1024)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+		
+	def GET(self, recurso):
+		self.tipo = T.CONFIRMABLE.value
+		self.code = CODE_0.GET.value
+		self.payload = b''
+		self.metodo = 'direto'
+		self.porta = int(5683)
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
+		data, addr = self.sock.recvfrom(1024)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+	
+	def POST(self, recurso, server_adress, port, entrada):
+		self.tipo = T.CONFIRMABLE.value
+		self.code = CODE_0.POST.value
+		self.payload = entrada.encode()
+		self.caminho = server_adress
+		self.porta = port
+		self.metodo = 'separado'
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
+		data, addr = self.sock.recvfrom(1024)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+		
+	def POST(self, recurso, entrada):
+		self.tipo = T.CONFIRMABLE.value
+		self.code = CODE_0.POST.value
+		self.payload = entrada.encode()
+		self.metodo = 'direto'
+		self.porta = int(5683)
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
+		data, addr = self.sock.recvfrom(1024)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+		
+		
+	def PUT(self, recurso, server_adress, port, entrada):
+		self.tipo = T.CONFIRMABLE.value
+		self.code = CODE_0.PUT.value
+		self.payload = entrada.encode()
+		self.caminho = server_adress
+		self.porta = port
+		self.metodo = 'separado'
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
+		data, addr = self.sock.recvfrom(1024)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+		
+	def PUT(self, recurso, entrada):
+		self.tipo = T.CONFIRMABLE.value
+		self.code = CODE_0.PUT.value
+		self.payload = entrada.encode()
+		self.metodo = 'direto'
+		self.porta = int(5683)
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
+		data, addr = self.sock.recvfrom(1024)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+		
+	def DELETE(self, recurso, server_adress, port)
+		self.tipo = T.CONFIRMABLE.value
+		self.code = CODE_0.GET.value
+		self.payload = b''
+		self.caminho = server_adress
+		self.porta = port
+		self.metodo = 'separado'
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
 		data, addr = self.sock.recvfrom(1024)
 		
 		resposta = self.recebe(data)
 		print(resposta)
 
-	def POST(self, uri_path, server_adress, port):
-		self.tipo = T.CONFIRMABLE.value
-		self.code = CODE_0.POST.value
-		self.optionsdelta = DELTA.URI_PATH.value
-		self.optionslength = len(uri_path)
-		self.options = uri_path
-		self.payload = b'XABLAU'
-		self.QUADRO()
-		print(self.quadro)
-
-		self.sock.sendto(self.quadro, (server_adress, port))
-		data, addr = self.sock.recvfrom(1024)
-		print(data)
-		print(addr)
-
-	def PUT(self, uri_path, server_adress, port):
-		self.tipo = T.CONFIRMABLE.value
-		self.code = CODE_0.PUT.value
-		self.optionsdelta = DELTA.URI_PATH.value
-		self.optionslength = len(uri_path)
-		self.options = uri_path
-		self.payload = b'XABLAU'
-		self.QUADRO()
-		print(self.quadro)
-
-		self.sock.sendto(self.quadro, (server_adress, port))
-		data, addr = self.sock.recvfrom(1024)
-		print(data)
-		print(addr)
-
-	def DELETE(self, uri_path, server_adress, port):
+	def DELETE(self, recurso):
 		self.tipo = T.CONFIRMABLE.value
 		self.code = CODE_0.DELETE.value
-		self.optionsdelta = DELTA.URI_PATH.value
-		self.optionslength = len(uri_path)
-		self.options = uri_path
-		self.QUADRO()
-		print(self.quadro)
-
-		self.sock.sendto(self.quadro, (server_adress, port))
+		self.payload = b''
+		self.metodo = 'direto'
+		self.porta = int(5683)
+		self.QUADRO(recurso)
+		
+		self.sock.sendto(self.quadro, (self.caminho, self.porta))
 		data, addr = self.sock.recvfrom(1024)
-		print(data)
-		print(addr)
+		
+		resposta = self.recebe(data)
+		print(resposta)
+		
